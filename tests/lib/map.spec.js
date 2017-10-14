c0455550ba5dcda927d068049edcd13116ef5e5c
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-14 13:17:01 +0100

    test fixes

--- tests/lib/map.spec.js
+++ tests/lib/map.spec.js
@@ -3,53 +3,60 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 const testData = require('../../data/stub.json');
 
-let map;
-let fetchStub;
+describe('The map library', () => {
 
-beforeEach(() => {
- 	fetchStub = sinon.stub();
-	map = proxyquire('../../lib/map', {
-		'isomorphic-fetch': fetchStub.returns(Promise.resolve({json: function () {return Promise.resolve(testData);}}))
+	let map;
+	let fetchStub;
+
+	beforeEach(() => {
+	 	fetchStub = sinon.stub();
+		map = proxyquire('../../lib/map', {
+			'isomorphic-fetch': fetchStub.returns(Promise.resolve({json: function () {return Promise.resolve(testData);}}))
+		});
+
+		// process.env.ACCEPTABLE_RADIUS = 2000;
 	});
 
-	// process.env.ACCEPTABLE_RADIUS = 2000;
-});
+	afterEach(() => {
+		fetchStub.reset();
+		// delete process.env.ACCEPTABLE_RADIUS;
+	});
 
-afterEach(() => {
-	fetchStub.reset();
-	// delete process.env.ACCEPTABLE_RADIUS;
-});
-
-describe('The map library', () => {
 	it('should call fetch if no TEST env is set', async () => {
 		await map();
 		sinon.assert.called(fetchStub);
 	});
-	it('should not call fetch if TEST env is set', () => {
+
+	it('should not call fetch if TEST env is set', async () => {
 		process.env.TEST = 'true';
-		map();
+		await map();
 		sinon.assert.notCalled(fetchStub);
 		delete process.env.TEST;
 	});
+
 	it('should return a promise', () => {
 		assert.equal('function', typeof map().then);
 	});
+
 	it('should return an array', async () => {
 		const mons = await map();
 		assert(mons.length > 0);
 	});
+
 	it('should return an array of objects with a name', async () => {
 		const mons = await map();
 		const firstMon = mons[0];
 		assert(firstMon.name !== undefined);
 		assert('string', typeof firstMon.name);
 	});
+
 	it('should return an array of objects with a despawn time', async () => {
 		const mons = await map();
 		const firstMon = mons[0];
 		assert(firstMon.despawn !== undefined);
 		assert('string', typeof firstMon.despawn);
 	});
+
 	it('should return an array of objects with a numeric distance', async () => {
 		const mons = await map();
 		const firstMon = mons[0];
--- tests/util/geo.spec.js
+++ tests/util/geo.spec.js
@@ -1,15 +1,16 @@
 const assert = require('assert');
 const geo = require('../../util/geo');
 
-// Eiffel Tower
-const lat1 = 48.858093;
-const lon1 = 2.294694;
-
-// Big Ben
-const lat2 = 51.5007;
-const lon2 = 0.1246;
-
 describe('The geo utility', () => {
+
+	// Eiffel Tower
+	const lat1 = 48.858093;
+	const lon1 = 2.294694;
+
+	// Big Ben
+	const lat2 = 51.5007;
+	const lon2 = 0.1246;
+
 	it('should a N distance between two lat/long pairs', () => {
 		const distance = geo.getDistance(lat1,lon1,lat2,lon2);
 		assert(typeof distance, 'number');
