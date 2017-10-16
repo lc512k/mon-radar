fdeaf8ef907dcc2a29fcf5b64214c7cc8776e7ad
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-16 19:44:29 +0100

    logs

--- scripts/server-push.js
+++ scripts/server-push.js
@@ -3,7 +3,6 @@ const env = require('node-env-file');
 const fetchMons = require('../server/lib/map');
 const mongoClient = require('../server/lib/mongo');
 const SubscriptionModel = require('../server/models/sub');
-// const path = require('path');
 
 if (!process.env.PRODUCTION) {
 	env(__dirname + '/../.env');
@@ -21,29 +20,15 @@ async function init () {
 
 	let dataJSON;
 
-	// const subsPath = path.join(__dirname, '..', '/data/subs.json');
-	// console.log('READING FROM', subsPath);
-	// console.log('READING FROM PATH', process.env.DB_PATH);
-	// const data = fs.readFileSync(process.env.DB_PATH, 'utf8');
-	// const dataJSON = JSON.parse(data);
-
-	// console.log(SubscriptionModel)
-
-
 	mongoClient.then(() => {
 		console.log('mongo client done');
 		SubscriptionModel.find({}, async (err, result) => {
 			dataJSON = result;
 
-			// console.log(c)
-			// console.log('subs');
 			console.log(dataJSON);
 
 			for (let sub of dataJSON) {
-				console.log(sub)
-				// const uuid = sub._id;
 
-				// const sub = dataJSON[uuid];
 				const pushSubscription = sub.subscription;
 
 				// TODO if duplicate locations (or close enough) fire off a single request
--- server/lib/mongo.js
+++ server/lib/mongo.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+console.log('process.env.MONGODB_URI', process.env.MONGODB_URI);
 module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/monradar');
