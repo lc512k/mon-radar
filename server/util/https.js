60b2aaaffc79afe7b7ac219d5fb84091ec1da8bf
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-18 19:38:10 +0100

    https logs

--- server/util/https.js
+++ server/util/https.js
@@ -1,7 +1,7 @@
-// module.exports = function (req, res, next) {
-// 	console
-//     if (!req.secure && process.env.PRODUCTION) {
-//         return res.redirect('https://' + req.get('host') + req.url);
-//     }
-//     next();
-// };
+module.exports = function (req, res, next) {
+	console.log('not redirecting to https');
+ //    if (!req.secure && process.env.PRODUCTION) {
+ //        return res.redirect('https://' + req.get('host') + req.url);
+ //    }
+ //    next();
+};
