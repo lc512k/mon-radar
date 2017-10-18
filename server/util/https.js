e540b1783a6817c139aeb50ec9c255bc2960ed2e
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-18 19:39:20 +0100

    next()

--- server/util/https.js
+++ server/util/https.js
@@ -3,5 +3,5 @@ module.exports = function (req, res, next) {
  //    if (!req.secure && process.env.PRODUCTION) {
  //        return res.redirect('https://' + req.get('host') + req.url);
  //    }
- //    next();
+    next();
 };
