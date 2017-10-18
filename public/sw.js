932dbe89a39b6c10b1caa047c0da67a17d72f6c0
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-18 21:52:48 +0100

    activate

--- public/sw.js
+++ public/sw.js
@@ -69,50 +69,50 @@ self.addEventListener('pushsubscriptionchange', function (event) {
 
 // OFFLINE
 self.addEventListener('install', (e) => {
-self.skipWaiting();
- e.waitUntil(
-	 caches.open('monradar').then((cache) => {
-		 return cache.addAll([
-			 '/lib/js.cookie.js',
-			 '/lib/material.min.css',
-			 '/lib/material.min.js',
-			 '/lib/uuid-v4.js',
-			 '/img/25.png',
-			 '/img/26.png',
-			 '/img/45.png',
-			 '/img/63.png',
-			 '/img/64.png',
-			 '/img/65.png',
-			 '/img/68.png',
-			 '/img/71.png',
-			 '/img/108.png',
-			 '/img/113.png',
-			 '/img/131.png',
-			 '/img/143.png',
-			 '/img/147.png',
-			 '/img/148.png',
-			 '/img/149.png',
-			 '/img/152.png',
-			 '/img/153.png',
-			 '/img/154.png',
-			 '/img/155.png',
-			 '/img/156.png',
-			 '/img/157.png',
-			 '/img/179.png',
-			 '/img/180.png',
-			 '/img/181.png',
-			 '/img/191.png',
-			 '/img/192.png',
-			 '/img/201.png',
-			 '/img/236.png',
-			 '/img/237.png',
-			 '/img/242.png',
-			 '/img/246.png',
-			 '/img/247.png',
-			 '/img/248.png'
-		 ]);
-	 })
- );
+	self.skipWaiting();
+	e.waitUntil(
+		caches.open('monradarv2').then((cache) => {
+			return cache.addAll([
+				'/lib/js.cookie.js',
+				'/lib/material.min.css',
+				'/lib/material.min.js',
+				'/lib/uuid-v4.js',
+				'/img/25.png',
+				'/img/26.png',
+				'/img/45.png',
+				'/img/63.png',
+				'/img/64.png',
+				'/img/65.png',
+				'/img/68.png',
+				'/img/71.png',
+				'/img/108.png',
+				'/img/113.png',
+				'/img/131.png',
+				'/img/143.png',
+				'/img/147.png',
+				'/img/148.png',
+				'/img/149.png',
+				'/img/152.png',
+				'/img/153.png',
+				'/img/154.png',
+				'/img/155.png',
+				'/img/156.png',
+				'/img/157.png',
+				'/img/179.png',
+				'/img/180.png',
+				'/img/181.png',
+				'/img/191.png',
+				'/img/192.png',
+				'/img/201.png',
+				'/img/236.png',
+				'/img/237.png',
+				'/img/242.png',
+				'/img/246.png',
+				'/img/247.png',
+				'/img/248.png'
+			]);
+		})
+	);
 });
 
 self.addEventListener('fetch', (event) => {
@@ -121,6 +121,24 @@ self.addEventListener('fetch', (event) => {
 		caches.match(event.request).then((response) => {
 			console.log('cache hit?', response);
 			return response || fetch(event.request);
+		})
+	);
+});
+
+self.addEventListener('activate', function (event) {
+	console.log('Activating new service worker...');
+
+	const cacheWhitelist = ['monradarv2'];
+
+	event.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames.map(function (cacheName) {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
 		})
 	);
 });
