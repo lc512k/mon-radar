aa198af1ee34710c90e6e3fb63580a288bbbc78c
Laura Carvajal <laura.carvajal@gmail.com>
2017-10-22 20:43:05 +0100

    remove test project

--- my-project/handler.js
+++
@@ -1,7 +0,0 @@
-const serverPush = require('./scripts/server-push');
-
-export const hello = async (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const response = await serverPush('⚡');
-  callback(null, response);
-};
--- my-project/server/lib/mongo.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/monradar');
